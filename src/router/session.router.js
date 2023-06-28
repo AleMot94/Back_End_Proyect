d185afcb828e9d95eb25c6b2019e9ac6da49f7a7
AleMot94 <Alector666@gmai.com>
2023-06-28 13:25:20 -0300

    ejemplo session con login

--- src/router/session.router.js
+++ src/router/session.router.js
@@ -1,21 +1,21 @@
 import express from "express";
 
 export const routerSession = express.Router();
-
+// http://localhost:8080/session/login?username=pepe&password=pepepass
 routerSession.get("/set-session", (req, res) => {
   if (req.session.cont) {
     req.session.cont++;
     res.send("visitas " + req.session.cont);
   } else {
     req.session.cont = 1;
-    req.session.nombre = "tu vieja";
-    req.session.isAdmin = true;
     res.send("visitas " + req.session.cont);
   }
 });
 
 routerSession.get("/", (req, res) => {
-  res.send("cookies");
+  console.log("ID session : " + req.sessionID);
+  console.log(req.session);
+  res.send("datos de la session en consola del servidor");
 });
 
 routerSession.get("/logout", (req, res) => {
@@ -25,4 +25,14 @@ routerSession.get("/logout", (req, res) => {
     }
     res.send("logout success");
   });
+});
+
+routerSession.get("/login", (req, res) => {
+  const { username, password } = req.query;
+  if (username !== "pepe" || password !== "pepepass") {
+    return res.send("login failed");
+  }
+  req.session.user = username;
+  req.session.admin = false;
+  res.send("login success");
 });
